a4e9f8623a07704d712ed768934d712be8c3de54
Michael Herwig <develop@michael-herwig.de>
2022-07-09 14:27:16 +0200

    add path input parameter

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: (c) 2022, Michael Herwig <contact@michael-herwig.de>
+'use strict';
 
 const core = require('@actions/core');
 const github = require('@actions/github');
+const args = require('yargs').argv;
 
 (async () => {
   try {
-    core.notice('Hello World!');
+    core.notice(`${args.path}`);
   }
   catch (error) {
     core.setFailed(error.message);
