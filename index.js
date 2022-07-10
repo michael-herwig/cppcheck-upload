fda1cc36d9eab9fa179de0ac6953f96b26e49a01
Michael Herwig <develop@michael-herwig.de>
2022-07-10 08:06:07 +0200

    add plist conversion

--- index.js
+++ index.js
@@ -1,14 +1,72 @@
-// SPDX-License-Identifier: MIT
+// // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: (c) 2022, Michael Herwig <contact@michael-herwig.de>
 'use strict';
 
 const core = require('@actions/core');
 const github = require('@actions/github');
 const args = require('yargs').argv;
+const fs = require('fs');
+const path = require('node:path');
+const plist = require('plist');
+const { Octokit } = require("@octokit/rest");
+
+async function collectPlistFiles(dir) {
+  return await fs.promises.readdir(dir).then(async files => {
+    return await Promise.all(files.map(async file => {
+      const filepath = path.join(dir, file);
+      const filestats = await fs.promises.stat(filepath);
+      if (filestats.isFile()) {
+        if (filepath.endsWith('.plist')) {
+          return [filepath];
+        }
+      } else if (filestats.isDirectory()) {
+        return await collectPlistFiles(filepath);
+      }
+      return [];
+    })).then(filelists => { return filelists.flat(); });
+  });
+}
+
+async function importPlist(file) {
+  return await fs.promises.readFile(file, { encoding: 'utf8' }).then(async content => {
+    const { files, diagnostics } = plist.parse(content);
+    return diagnostics.map(diagnostic => {
+      return {
+        path: files[diagnostic.location.file],
+
+        start_line: diagnostic.location.line,
+        end_line: diagnostic.location.line,
+        start_column: diagnostic.location.col,
+        end_column: diagnostic.location.col,
+
+        annotation_level: 'warning',
+        message: diagnostic.description,
+        title: `${diagnostic.category}: ${diagnostic.check_name}`
+      }
+    });
+  });
+}
 
 (async () => {
   try {
-    core.notice(`${args.path}`);
+    const files = await collectPlistFiles(args.path);
+    const annotations = await Promise.all(files.map(async (file) => {
+      return await importPlist(file);
+    })).then(annotations => { return annotations.flat(); });
+
+    await Octokit.rest.checks.create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      name: 'Cppcheck Upload',
+      status: 'completed',
+      conclusion: 'success',
+
+      output: {
+        title: `TODO Small Feedback`,
+        summary: `TODO Build small summary!`,
+        annotations: annotations
+      }
+    });
   }
   catch (error) {
     core.setFailed(error.message);
